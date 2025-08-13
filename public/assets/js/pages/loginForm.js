4556712548f949708fe9be1aa493f48270035624
SoyGenkary <SoyGenkary@gmail.com>
2025-08-12 20:20:36 -0400

    Refactorizacion loginForm.js

--- public/assets/js/pages/loginForm.js
+++ public/assets/js/pages/loginForm.js
@@ -5,31 +5,9 @@ import {
 import { ntfProcessError, ntfProcessSuccessful } from "./../utils/utils.js";
 import { apiRequest } from "./../API/api.js";
 
-// Espera a que el contenido del DOM esté completamente cargado y parseado
-document.addEventListener("DOMContentLoaded", () => {
-  // Selección de elementos del DOM
-  const loginContainer = document.getElementById("login-container");
-  const registerContainer = document.getElementById("register-container");
+function eventRegisterLink(loginContainer, registerContainer) {
   const showRegisterLink = document.getElementById("show-register");
-  const showLoginLink = document.getElementById("show-login");
-  const registerForm = document.getElementById("register-form");
-  const loginForm = document.getElementById("login-form");
 
-  const cedulaInput = document.getElementById("register-cedula");
-  const phoneInput = document.getElementById("register-telefono");
-  const emailInput = document.getElementById("register-email");
-  const nameInput = document.getElementById("register-name");
-
-  if (cedulaInput && phoneInput) {
-    cedulaInput.addEventListener("input", (e) => {
-      e.target.value = formatInputDNI(e.target.value);
-    });
-    phoneInput.addEventListener("input", (e) => {
-      e.target.value = formatInputPhoneNumber(e.target.value);
-    });
-  }
-
-  // Event listener para mostrar el formulario de registro
   if (showRegisterLink) {
     showRegisterLink.addEventListener("click", (e) => {
       e.preventDefault();
@@ -39,95 +17,57 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   }
+}
 
-  // Event listener para mostrar el formulario de inicio de sesión
-  if (showLoginLink) {
-    showLoginLink.addEventListener("click", (e) => {
-      e.preventDefault();
-      if (loginContainer && registerContainer) {
-        registerContainer.classList.remove("active"); // Ocultar formulario de registro
-        loginContainer.classList.add("active"); // Mostrar formulario de login
-      }
+function validationInput(input, message) {
+  if (input && input.value.trim() === "") {
+    e.preventDefault();
+    ntfProcessError("¡Ey!", message);
+    input.focus();
+  }
+}
+
+function eventFormatInput(cedulaInput, phoneInput) {
+  if (cedulaInput && phoneInput) {
+    cedulaInput.addEventListener("input", (e) => {
+      e.target.value = formatInputDNI(e.target.value);
+    });
+    phoneInput.addEventListener("input", (e) => {
+      e.target.value = formatInputPhoneNumber(e.target.value);
     });
   }
+}
 
-  // Event listener para el envío del formulario de login
-  if (loginForm) {
-    loginForm.addEventListener("submit", async (e) => {
-      const loginInput = document.getElementById("loginForm");
-      const passwordInput = document.getElementById("login-password");
-
-      // Validacion para el email del usuario
-      if (loginInput && loginInput.value.trim() === "") {
-        e.preventDefault();
-        ntfProcessError("¡Ey!", "Debes ingresar email.");
-        loginInput.focus();
-        return;
-      }
-
-      // Validación de que las contraseñas coincidan
-      if (passwordInput && passwordInput.value.trim() === "") {
-        e.preventDefault();
-        ntfProcessError("¡Ey!", "Debes Ingresar una contraseña");
-        passwordInput.focus();
-        return;
-      }
-
-      e.preventDefault();
-
-      const formData = new FormData(loginForm);
-      formData.append("section", "user");
-      formData.append("action", "login");
-      const response = await apiRequest(formData);
-
-      if (!response["success"]) {
-        ntfProcessError("Oops...", response["message"]);
-      } else {
-        location.assign("./layout.php");
-      }
-    });
-  }
-
-  // Event listener para el envío del formulario de registro
+function eventSubmitRegisterForm(
+  registerForm,
+  loginContainer,
+  registerContainer,
+  cedulaInput,
+  phoneInput,
+  emailInput,
+  nameInput
+) {
   if (registerForm) {
     registerForm.addEventListener("submit", async (e) => {
+      e.preventDefault();
+
       const passwordInput = document.getElementById("register-password");
       const confirmPasswordInput = document.getElementById(
         "register-confirm-password"
       );
-      const masterKeyInput = document.getElementById("register-master-key"); // Nuevo campo
+      const masterKeyInput = document.getElementById("register-master-key");
 
       // Validacion para la Cedula del usuario
-      if (cedulaInput && cedulaInput.value.trim() === "") {
-        e.preventDefault();
-        ntfProcessError("¡Ey!", "Debes ingresar tu documento de identidad.");
-        cedulaInput.focus();
-        return;
-      }
+      validationInput(cedulaInput, "Debes ingresar tu documento de identidad.");
 
       // Validacion para el telefono del usuario
-      if (phoneInput && phoneInput.value.trim() === "") {
-        e.preventDefault();
-        ntfProcessError("¡Ey!", "Debes ingresar tu número de telefono.");
-        phoneInput.focus();
-        return;
-      }
+      validationInput(phoneInput, "Debes ingresar tu número de telefono.");
 
       // Validacion para el email del usuario
-      if (emailInput && emailInput.value.trim() === "") {
-        e.preventDefault();
-        ntfProcessError("¡Ey!", "Debes ingresar email.");
-        emailInput.focus();
-        return;
-      }
+      validationInput(emailInput, "Debes ingresar tu email.");
 
       // Validacion para el nombre del usuario
-      if (nameInput && nameInput.value.trim() === "") {
-        e.preventDefault();
-        ntfProcessError("¡Ey!", "Debes ingresar un nombre de usuario.");
-        nameInput.focus();
-        return;
-      }
+      validationInput(nameInput, "Debes ingresar tu nombre de usuario.");
 
       // Validación de que las contraseñas coincidan
       if (passwordInput && confirmPasswordInput) {
@@ -142,28 +82,126 @@ document.addEventListener("DOMContentLoaded", () => {
       }
 
       // Validación para la Clave Maestra
-      if (masterKeyInput && masterKeyInput.value.trim() === "") {
-        e.preventDefault();
-        ntfProcessError("¡Ey!", "Por favor, ingrese la Clave Maestra.");
-        masterKeyInput.focus();
-        return;
-      }
+      validationInput(masterKeyInput, "Debes ingresar la Clave Maestra.");
 
+      // Logica de envio de formulario
+      registerRequestAPI(registerForm, loginContainer, registerContainer);
+    });
+  }
+}
+
+async function registerRequestAPI(
+  registerForm,
+  loginContainer,
+  registerContainer
+) {
+  // Llamada a la api
+  const formData = new FormData(registerForm);
+  formData.append("section", "user");
+  formData.append("action", "add");
+  const response = await apiRequest(formData);
+
+  // Muestreo de respuesta
+  if (response["success"]) {
+    registerForm.reset();
+    registerContainer.classList.remove("active");
+    loginContainer.classList.add("active");
+    ntfProcessSuccessful("Proceso Exitoso!", response["message"]);
+  } else {
+    ntfProcessError("Oops...", response["message"]);
+  }
+}
+
+function eventsRegisterForm(registerContainer, loginContainer) {
+  const cedulaInput = document.getElementById("register-cedula");
+  const phoneInput = document.getElementById("register-telefono");
+  const emailInput = document.getElementById("register-email");
+  const nameInput = document.getElementById("register-name");
+  const registerForm = document.getElementById("register-form");
+
+  // Event listener para mostrar el formulario de registro
+  eventRegisterLink(loginContainer, registerContainer);
+
+  // Event listener para formatear automaticamente algunos inputs
+  eventFormatInput(cedulaInput, phoneInput);
+
+  // Event listener para el envío del formulario de registro
+  eventSubmitRegisterForm(
+    registerForm,
+    loginContainer,
+    registerContainer,
+    cedulaInput,
+    phoneInput,
+    emailInput,
+    nameInput
+  );
+}
+
+function eventShowForm(loginContainer, registerContainer) {
+  const showLoginLink = document.getElementById("show-login");
+
+  if (showLoginLink) {
+    showLoginLink.addEventListener("click", (e) => {
       e.preventDefault();
 
-      const formData = new FormData(registerForm);
-      formData.append("section", "user");
-      formData.append("action", "add");
-      const response = await apiRequest(formData);
-
-      if (response["success"]) {
-        registerForm.reset();
-        registerContainer.classList.remove("active");
-        loginContainer.classList.add("active");
-        ntfProcessSuccessful("Proceso Exitoso!", response["message"]);
-      } else {
-        ntfProcessError("Oops...", response["message"]);
+      if (loginContainer && registerContainer) {
+        registerContainer.classList.remove("active"); // Ocultar formulario de registro
+        loginContainer.classList.add("active"); // Mostrar formulario de login
       }
     });
   }
+}
+
+async function loginRequestAPI(loginForm) {
+  const formData = new FormData(loginForm);
+  formData.append("section", "user");
+  formData.append("action", "login");
+  const response = await apiRequest(formData);
+
+  if (!response["success"]) {
+    ntfProcessError("Oops...", response["message"]);
+  } else {
+    location.assign("./layout.php");
+  }
+}
+
+function eventSubmitLoginForm(loginForm) {
+  if (loginForm) {
+    loginForm.addEventListener("submit", async (e) => {
+      e.preventDefault();
+
+      const loginInput = document.getElementById("loginForm");
+      const passwordInput = document.getElementById("login-password");
+
+      // Validacion para el email del usuario
+      validationInput(loginInput, "Debes ingresar email.");
+
+      // Validación de que las contraseñas coincidan
+      validationInput(passwordInput, "Debes Ingresar una contraseña");
+
+      // Logica de envio de formulario
+      loginRequestAPI(loginForm);
+    });
+  }
+}
+
+function eventsLoginForm(loginContainer, registerContainer, loginForm) {
+  // Event listener para mostrar el formulario de inicio de sesión
+  eventShowForm(loginContainer, registerContainer);
+
+  // Event listener para el envío del formulario de inicio de sesión
+  eventSubmitLoginForm(loginForm);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Selección de elementos del DOM
+  const loginContainer = document.getElementById("login-container");
+  const registerContainer = document.getElementById("register-container");
+  const loginForm = document.getElementById("login-form");
+
+  // Eventos relacionados al registro
+  eventsRegisterForm(registerContainer, loginContainer);
+
+  // Eventos relacionados con el login
+  eventsLoginForm(loginContainer, registerContainer, loginForm);
 });
